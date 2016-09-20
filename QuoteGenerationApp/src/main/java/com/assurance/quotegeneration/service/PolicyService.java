3bcea6165f2f84c4d54b04eece0d5a54d9444a9c
srinivas0190 <srinivas.kf563@gmail.com>
2016-09-20 11:02:32 -0500

    removed for update the change

--- QuoteGenerationApp/src/main/java/com/assurance/quotegeneration/service/PolicyService.java
+++
@@ -1,6 +0,0 @@
-package com.assurance.quotegeneration.service;
-
-//@Service("policyService")
-public class PolicyService {
-
-}
--- QuoteGenerationApp/src/main/java/com/assurance/quotegeneration/service/QuoteHandler.java
+++
@@ -1,6 +0,0 @@
-package com.assurance.quotegeneration.service;
-
-//@Service("quoteHandler")
-public class QuoteHandler {
-
-}
